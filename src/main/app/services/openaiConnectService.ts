4aa3da597c1ed1ec1c547c662225391c70e3afa7
sumds <catsumd@gmail.com>
2024-02-23 19:41:04 +0530

    added file upload part

--- src/main/app/services/openaiConnectService.ts
+++ src/main/app/services/openaiConnectService.ts
@@ -1,13 +1,11 @@
 import OpenAI from "openai";
+import * as fs from 'fs';
 
 import { ReadLine, createInterface } from "readline";
 
-
-
 export class openaiConnectService {
-
   private openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY,
   });
 
   private readline: ReadLine = createInterface({
@@ -15,8 +13,7 @@ export class openaiConnectService {
     output: process.stdout,
   });
 
-
-  private  askQuestion(question: string): Promise<string> {
+  private askQuestion(question: string): Promise<string> {
     return new Promise((resolve, reject) => {
       this.readline.question(question, (answer) => {
         resolve(answer);
@@ -24,30 +21,34 @@ export class openaiConnectService {
     });
   }
 
-
-  
   constructor() {
     console.log("openaiConnectService constructor");
   }
 
-
   public async initialise(): Promise<void> {
     try {
       console.log("inside assistant block");
-      // Creating the assistant
-      const mathTutorAssistant = await this.openai.beta.assistants.create({
-        name: "Math Tutor",
-        instructions:
-          "You are a personal math tutor. Write and run code to answer math questions.",
-        tools: [{ type: "code_interpreter" }],
-        model: "gpt-4-turbo-preview",
+
+      const file = await this.openai.files.create({
+        file: fs.createReadStream("tx_db_schema.sql"),
+        purpose: "assistants",
       });
 
-      const mathTutorAssistantId = mathTutorAssistant.id;
-      console.log("assistant id is:", mathTutorAssistantId);
+      // Creating the assistant
+      const dbqueryAssistant = await this.openai.beta.assistants.create({
+        name: "Database Expert",
+        instructions:
+          "You are an expert dba. Considering the File attached, you must generate SQL's to answer questions.",
+        tools: [{ type: "code_interpreter" }],
+        model: "gpt-4-turbo-preview",
+        file_ids: [file.id]
+      });
+
+      const dbqueryAssistantId = dbqueryAssistant.id;
+      console.log("assistant id is:", dbqueryAssistantId);
 
       console.log(
-        "\nHello there, I'm your personal math tutor. Ask some complicated questions.\n"
+        "\nHello there, I'm your database expert. Ask some complicated queries.\n"
       );
 
       const thread = await this.openai.beta.threads.create();
@@ -55,23 +56,31 @@ export class openaiConnectService {
       console.log("the thread id is:", threadId);
 
       let keepAsking = true;
-      
+
       while (keepAsking) {
-        const userQuestion = await this.askQuestion("\nWhat is your question? ");
+        const userQuestion = await this.askQuestion(
+          "\nWhat is your question? "
+        );
         await this.openai.beta.threads.messages.create(threadId, {
           role: "user",
           content: userQuestion,
         });
 
         const run = await this.openai.beta.threads.runs.create(threadId, {
-          assistant_id: mathTutorAssistantId,
+          assistant_id: dbqueryAssistantId,
         });
 
-        let runStatus = await this.openai.beta.threads.runs.retrieve(threadId, run.id);
+        let runStatus = await this.openai.beta.threads.runs.retrieve(
+          threadId,
+          run.id
+        );
 
         while (runStatus.status !== "completed") {
           await new Promise((resolve) => setTimeout(resolve, 2000));
-          runStatus = await this.openai.beta.threads.runs.retrieve(threadId, run.id);
+          runStatus = await this.openai.beta.threads.runs.retrieve(
+            threadId,
+            run.id
+          );
         }
 
         const messages = await this.openai.beta.threads.messages.list(threadId);
