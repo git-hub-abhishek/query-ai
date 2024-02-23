cc1f7cfc87983ac5a7b07cf08047e8a6d2cd5f4b
abhishek <abhishek.choudhury@email.com>
2024-02-23 15:34:43 +0000

    added basic front end

--- src/main/app/server.ts
+++ src/main/app/server.ts
@@ -1,12 +1,16 @@
 // esmodule top flag
 
 import * as express from "express";
+import * as path from "path";
 
 const app = express();
 const port = 3001;
+const frontend_path = process.env.FRONTEND_PATH || path.join(__dirname, '/assets/web');
+
+
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.sendFile(path.join(frontend_path, '/index.html'));
 });
 
 app.listen(port, () => {
