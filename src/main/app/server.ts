3dfe835a6bec0a879e9a8c843df905c6e1636fdd
abhishek <abhishek.choudhury@email.com>
2024-02-23 15:48:04 +0000

    Merge

--- src/main/app/server.ts
+++ src/main/app/server.ts
@@ -3,17 +3,19 @@
 import * as express from "express";
 import { SelectController } from "./controllers/select-controller";
 import { openaiConnectService } from "./services/openaiConnectService";
+import * as path from "path";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const frontend_path = path.join(__dirname, '/ass');
 
 
 app.get('/select', async (req, res) => {
     var  select = new SelectController(new openaiConnectService());
     await select.handleEvent(req, res);
-};
+});
 app.get('/', (req, res) => {
     res.sendFile(path.join(frontend_path, '/index.html'));
 });
